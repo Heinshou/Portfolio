3c24f62d2b541d5028cb45bee87ddf2d3a522b3f
Miguel <miguelecchavez@gmail.com>
2022-07-18 12:16:12 -0600

    #5

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import './Components/Nav/NavBar.css'
 import NavBar from './Components/Nav/NavBar'
 import AboutMe from './Components/About/AboutMe'
+import Skills from './Components/Skills/Skills'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div>
       <NavBar/>
       <AboutMe/>
+      <Skills/>
     </div>
   )
 }
---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Skills.css'
+
+const Skills = () => {
+  return (
+    <div className='skills__container'>
+        <h3 className='skills__tittle'>Skills</h3>
+    </div>
+  )
+}
+
+export default Skills
